ab3a96e1a3cd0a0699f888fed3d6ce3fe7ee3403
Paweł <84644073+Pawel-Lechki@users.noreply.github.com>
2023-08-20 14:27:54 +0200

    add fatch posts

--- src/pages/PostList.vue
+++ src/pages/PostList.vue
@@ -1,7 +1,19 @@
-<template>Post List</template>
+<template>
+  <pre>
+  {{ postList }}
+  </pre>
+</template>
 
-<script>
-  export default {}
+<script setup>
+  import { onMounted, ref } from "vue"
+
+  const postList = ref([])
+
+  onMounted(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => res.json())
+      .then((posts) => (postList.value = posts))
+  })
 </script>
 
 <style></style>
